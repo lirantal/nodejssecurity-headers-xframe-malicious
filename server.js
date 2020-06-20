591a53e04f80afe27597a8099648b4dc965d0dcb
Liran Tal <liran.tal@gmail.com>
2020-06-20 18:33:41 +0300

    server logs verbose

--- server.js
+++ server.js
@@ -18,5 +18,5 @@ app.use(express.static("public"));
 
 const server = http.createServer(app);
 server.listen(HTTP_PORT_NUMBER, function () {
-  console.log(`Server started on port: ${server.address().port}`);
+  console.log(`💀 ::Malicious:: Server started on port: ${server.address().port}`);
 });
